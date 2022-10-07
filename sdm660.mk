48b4f96939a3759fcb33ecac81470edf150e5b1e
dianlujitao <dianlujitao@lineageos.org>
2022-10-07 20:29:49 +0800

    sdm660-common: Nuke ATFWD [1/2]
    It's normally not used at all.
    
    Change-Id: Idb824b3b8c783b6ac962e604df05f9154beb4ed2
    Signed-off-by: Tiktodz <dotkit@electrowizard.me>

--- sdm660.mk
+++ sdm660.mk
@@ -375,7 +375,6 @@ PRODUCT_PACKAGES += \
 
 # Seccomp
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/seccomp/atfwd@2.0.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/atfwd@2.0.policy \
     $(LOCAL_PATH)/configs/seccomp/imsrtp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/imsrtp.policy \
     $(LOCAL_PATH)/configs/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
