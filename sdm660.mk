7800958c39768b28b55b3ac0692523b88ffa023b
Vivekachooz <vivekachooz@gmail.com>
2023-08-20 14:36:52 +0530

    sdm660-common: Explicitly build vendor.display.config@1.0
    Change-Id: Iee80ca6f1b784c32d3fc09caa97a978f9acff46b
    Signed-off-by: Tiktodz <dotkit@electrowizard.me>

--- sdm660.mk
+++ sdm660.mk
@@ -110,7 +110,9 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     android.frameworks.displayservice@1.0 \
-    android.frameworks.displayservice@1.0.vendor
+    android.frameworks.displayservice@1.0.vendor \
+    vendor.display.config@1.0 \
+    vendor.display.config@1.0.vendor
 
 # Doze mode
 PRODUCT_PACKAGES += \
