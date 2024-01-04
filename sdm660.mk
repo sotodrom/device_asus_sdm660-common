0358eacebaaeda26d7570df3c20e831f9022dc2a
Vivekachooz <vivekachooz@gmail.com>
2024-01-04 12:38:17 +0530

    Revert "sdm660-common: Enable OEM unlock and bootable image signing"
    This reverts commit 2d89ffbf089d0909a319105981b971ef43d52b9d.
    
    Change-Id: I16366aa81f147667de2ec7f6ade70e5525cbdc9b
    Signed-off-by: Tiktodz <dotkit@electrowizard.me>

--- sdm660.mk
+++ sdm660.mk
@@ -254,10 +254,6 @@ PRODUCT_PACKAGES += \
     com.android.nfc_extras \
     Tag
 
-# OEM Unlock reporting
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.oem_unlock_supported=1
-
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
@@ -423,11 +419,6 @@ PRODUCT_PACKAGES += \
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
-
-# Verity
-PRODUCT_SYSTEM_VERITY_PARTITION=/dev/block/bootdevice/by-name/system
-PRODUCT_VENDOR_VERITY_PARTITION=/dev/block/bootdevice/by-name/vendor
-$(call inherit-product, build/target/product/verity.mk)
 
 # Vibrator
 PRODUCT_PACKAGES += \
